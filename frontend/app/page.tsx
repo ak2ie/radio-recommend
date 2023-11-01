5560fd5587dd0882a1a8dfdd820ce7315a2c09d3
ak2ie <mail@ak2ie.net>
2023-11-01 21:11:49 +0900

    init frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Container, Box, Button } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Container>
+      <Box>
+        <Button variant='contained'>実行</Button>
+      </Box>
+    </Container>
+  )
+}
